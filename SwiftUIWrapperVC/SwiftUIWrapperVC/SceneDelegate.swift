63a168228dd678a77fbcae663d3c54cb764d7899
insub <vb15@kakao.com>
2023-06-03 10:56:45 +0900

    Update SceneDelegate.swift

--- SwiftUIWrapperVC/SwiftUIWrapperVC/SceneDelegate.swift
+++ SwiftUIWrapperVC/SwiftUIWrapperVC/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         
@@ -22,8 +23,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .init()
             .wrap()
         
-        let nc = UINavigationController(rootViewController: controller)
-        window?.rootViewController = nc
+        let nController = UINavigationController(rootViewController: controller)
+        window?.rootViewController = nController
         window?.makeKeyAndVisible()
     }
 
